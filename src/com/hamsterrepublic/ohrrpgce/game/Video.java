78ce8bf3aba28f0469d4b75cbcbfd76dec0d4f69
Ralph Versteegen <teeemcee@gmail.com>
2016-02-03 04:56:41 +1300

    Update both java and native SDL build to match sdl-android 77cd9b, for fix to emailFiles by using FileProvider

--- src/com/hamsterrepublic/ohrrpgce/game/Video.java
+++ src/com/hamsterrepublic/ohrrpgce/game/Video.java
@@ -50,6 +50,7 @@ import android.content.res.Resources;
 import android.content.res.AssetManager;
 import android.widget.Toast;
 import android.util.Log;
+import android.support.v4.content.FileProvider;
 
 import android.widget.TextView;
 import java.lang.Thread;
@@ -542,30 +543,44 @@ class DemoRenderer extends GLSurfaceView_SDL.Renderer
 		context.setScreenKeyboardHintMessage(s);
 	}
 
-	// Open an app to send an email with 0 to 3 file attachments: each file may be null
-	// The file must be readable from other processes.
-	// The alternative would be to use FileProvider, quite labourious.
+	// Open an app to send an email with 0 to 3 file attachments: each file may be null.
 	// Called from native code
 	public void emailFiles(String address, String subject, String message, String file1, String file2, String file3)
 	{
-		//Settings.nativeChmod(file1, 0755);
+		// The simplest way allow other apps to read our private files is this:
+		// It doesn't affect the file permissions, but rather
+		// sets permissions on the "abstract path associated with this URI".
+		// (It isn't possible to make a file in the files directory world-readable by setting file permissions)
+		//
+		// File file = new File(file1);
+		// file.setReadable(true, false);  //android 2.3+ only
+		// uris.add(Uri.fromFile(file));
+
+		// FileProvider will fail if a file is not in the files, cache, or external storage folder,
+		// as defined in res/xml/fileprovider_paths.xml
+		// But those pretty much cover anything that could come up except secondary external storage
+
+		String authority = context.getPackageName() + ".fileprovider";
+
 		ArrayList<Uri> uris = new ArrayList<Uri>();
-		if (file1 != null)
-			uris.add(Uri.fromFile(new File(file1)));
-		if (file2 != null)
-			uris.add(Uri.fromFile(new File(file2)));
-		if (file3 != null)
-			uris.add(Uri.fromFile(new File(file3)));
+		if (file1 != null) {
+			uris.add(FileProvider.getUriForFile(context, authority, new File(file1)));
+		}
+		if (file2 != null) {
+			uris.add(FileProvider.getUriForFile(context, authority, new File(file2)));
+		}
+		if (file3 != null) {
+			uris.add(FileProvider.getUriForFile(context, authority, new File(file3)));
+		}
 
 		Intent email = new Intent(Intent.ACTION_SEND_MULTIPLE);
+		email.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 		email.putExtra(Intent.EXTRA_EMAIL, new String[] {address});
 		email.putExtra(Intent.EXTRA_SUBJECT, subject);
 		email.putExtra(Intent.EXTRA_TEXT, message);
 		email.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
-		//email.putExtra(Intent.EXTRA_STREAM, fileUri);
-		//email.setType("message/rfc822");
 		email.setType("text/plain");
-		context.startActivity(Intent.createChooser(email, "Email the developer how?"));
+		context.startActivity(Intent.createChooser(email, "Email the game developer how?"));
 	}
 
 	public void startAccelerometerGyroscope(int started)
